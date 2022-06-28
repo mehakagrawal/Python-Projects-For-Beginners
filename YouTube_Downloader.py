87532835ff36a5094e6baa44b120a0b35947e504
Mehak Agrawal <mehaka2212@gmail.com>
2022-06-28 21:41:17 +0530

    Add YouTube Downloader

---
+++ YouTube_Downloader.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from pytube import YouTube
+
+root = Tk()
+root.geometry('600x300')
+root.resizable(0, 0)
+root.title("Mehak's YouTube Video Downloader")
+
+Label(root, text="YouTube Video Downloader", font='arial 30 bold').pack()
+
+link = StringVar
+
+Label(root, text="Paste the link of YouTube video here", font='arial 15 bold').place(x = 160, y = 60)
+
+link_enter = Entry(root, width=80, textvariable=link).place(x = 32, y = 90)
+
+def download():
+    url = YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+
+    Label(root, text="The video is downloaded!", font='arial 15').place(x =120, y = 210)
+
+    Button(root, text="Download", font='arial 15 bold', bg = 'black', padx=2, command=download).place(x = 180, y = 150)
+
+    root.mainloop()
